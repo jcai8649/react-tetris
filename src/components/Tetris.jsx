e7853c8fa186989790b35ce30e95ab10bdcbbc48
Jackson Cai <jcai8649@gmail.com>
2021-05-03 12:39:11 -0800

    Add score formula and speed level

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -8,6 +8,7 @@ import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 import { usePlayer } from "../hooks/usePlayer";
 import { useStage } from "../hooks/useStage";
 import { useInterval } from "../hooks/useInterval";
+import { useGameStatus } from "../hooks/useGameStatus";
 
 //Components
 import Stage from "./Stage";
@@ -19,7 +20,9 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer);
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] =
+    useGameStatus(rowsCleared);
 
   const movePlayer = (dir) => {
     if (!isCollision(player, stage, { x: dir, y: 0 }))
@@ -32,9 +35,19 @@ const Tetris = () => {
     setDropTime(1000);
     resetPlayer();
     setGameOver(false);
+    setScore(0);
+    setRows(0);
+    setLevel(1);
   };
 
   const drop = () => {
+    //Increase level when player has cleared 10 rows
+    if (rows > (level + 1) * 10) {
+      setLevel((prev) => prev + 1);
+
+      setDropTime(1000 / (level + 1) + 200);
+    }
+
     if (!isCollision(player, stage, { x: 0, y: 1 })) {
       updatePlayerPos({ x: 0, y: 1, collided: false });
     } else {
@@ -51,7 +64,7 @@ const Tetris = () => {
   const keyUp = ({ keyCode }) => {
     if (!gameOver) {
       if (keyCode === 40) {
-        setDropTime(1000);
+        setDropTime(1000 / (level + 1) + 200);
       }
     }
   };
@@ -97,9 +110,9 @@ const Tetris = () => {
             <Display gameOver={gameOver} text="Game Over" />
           ) : (
             <div>
-              <Display text="Score" />
-              <Display text="Rows" />
-              <Display text="Level" />
+              <Display text={`Score: ${score}`} />
+              <Display text={`Rows: ${rows}`} />
+              <Display text={`Level: ${level}`} />
             </div>
           )}
           <StartButton callback={startGame} />
---
+++ src/hooks/useGameStatus.js
@@ -0,0 +1,24 @@
+import { useState, useEffect, useCallback } from "react";
+
+const linePoints = [40, 100, 300, 1200];
+
+export const useGameStatus = (rowsCleared) => {
+  const [score, setScore] = useState(0);
+  const [rows, setRows] = useState(0);
+  const [level, setLevel] = useState(0);
+
+  const calcScore = useCallback(() => {
+    //We have score
+    if (rowsCleared > 0) {
+      //Orginal Tetris score formula
+      setScore((prev) => prev + linePoints[rowsCleared - 1] * (level + 1));
+      setRows((prev) => prev + rowsCleared);
+    }
+  }, [level, rowsCleared]);
+
+  useEffect(() => {
+    calcScore();
+  }, [calcScore, rowsCleared, score]);
+
+  return [score, setScore, rows, setRows, level, setLevel];
+};
