b4454f51f0f3cb8878cd26cea137f68a76008e15
Jackson Cai <jcai8649@gmail.com>
2021-05-27 12:39:11 -0800

    Fix deployment issue

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 //Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
